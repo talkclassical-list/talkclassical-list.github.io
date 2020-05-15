90dc61ede452118fb6312415ee263e136497bad4
NBonaparte <nbonaparte@protonmail.com>
2020-05-15 14:45:33 -0700

    wip: add navbar stuff

--- public/main.js
+++ public/main.js
@@ -10,3 +10,18 @@ for (const tier of tiers) {
 		}
 	});
 }
+
+document.getElementById("tier-expand").addEventListener("click", function() {
+	let shrunk = this.classList.toggle("icon-arrows-expand-vertical1");
+	this.classList.toggle("icon-arrows-shrink-vertical1");
+	for (const tier of tiers) {
+		let tier_items = tier.nextElementSibling;
+		if (shrunk) {
+			tier.classList.remove("active");
+			tier_items.style.maxHeight = null;
+		} else {
+			tier.classList.add("active");
+			tier_items.style.maxHeight = tier_items.scrollHeight + "px";
+		}
+	}
+});
