5498f6f3f26a3ff152545861368050093b7c9a14
NBonaparte <nbonaparte@protonmail.com>
2020-05-16 15:16:17 -0700

    stats: add tier slider for date chart

--- public/stats/stats.js
+++ public/stats/stats.js
@@ -54,20 +54,30 @@ fetch("/list.json")
 			showLine: false,
 			lineSmooth: Chartist.Interpolation.step(),
 		},
+		[
+			["screen and (max-width: 80.0rem)", {
+				axisX: {
+					labelInterpolationFnc: function(value, index) {
+						return value % 100 === 0 ? value : null;
+					}
+				}
+			}],
+		]
 		);
+		let tier_max = Math.max.apply(Math, a.map(w => w.tier));
+		let tier_slider = createSlider("tier-range", [1, tier_max + 1], 1, [1, ...[...Array(11).keys()].map(x => (x + 1) * 10), tier_max + 1], 100/(tier_max + 1));
+		tier_slider.noUiSlider.on("set", new_tiers => {
+			let [tier_slide_min, tier_slide_max] = new_tiers;
+			let filtered = a.filter(w => w.tier >= tier_slide_min && w.tier <= tier_slide_max);
+			let date_arr = filtered.map(w => w.raw_yr !== undefined && w.raw_yr.includes("cent") ? undefined : w.year).filter(e => e !== undefined);
+			let date_min = Math.min.apply(Math, date_arr);
+			let date_max = Math.max.apply(Math, date_arr);
+			let date = [...Array(date_max - date_min + 1).keys()].map(i => i + date_min).filter(i => i % 10 === 0);
+			let date_ct = date.map(d => date_arr.filter(i => Math.floor(i/10)*10 === d).length);
 
-		let date_min_rd = Math.floor(date_min/100)*100;
-		let date_slider = createSlider("date-range", [date_min_rd, date_max], 20, [...Array(11).keys()].map(x => (x + date_min_rd/100) * 100), 2000/(date_max - date_min_rd));
-		date_slider.noUiSlider.on("set", new_dates => {
-			let [date_slide_min, date_slide_max] = new_dates;
-			let date_min_idx = date.indexOf(Math.floor(date_slide_min));
-			date_min_idx = date_min_idx === -1 ? 0 : date_min_idx;
-			let date_max_idx = date.indexOf(Math.floor(date_slide_max));
-			date_max_idx = date_max_idx === -1 ? date.length-1 : date_max_idx;
-			console.log(date_slide_min, date_slide_max, date_min_idx, date_max_idx);
 			date_chart.update({
-				labels: date.slice(date_min_idx, date_max_idx),
-				series: [date_ct.slice(date_min_idx, date_max_idx)],
+				labels: date,
+				series: [date_ct],
 			});
 		});
 
