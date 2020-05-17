15db753224be7cb530a2e55239dfc9ceb205950a
NBonaparte <nbonaparte@protonmail.com>
2020-05-16 20:36:16 -0700

    fix(list): set scroll to same tier when collapsed

--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
+function isElementInViewport (el) {
+	let rect = el.getBoundingClientRect();
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+}
+
 var tiers = document.getElementsByClassName("tier");
 for (const tier of tiers) {
 	tier.addEventListener("click", function() {
@@ -17,6 +27,7 @@ tier_expand.addEventListener("click", function() {
 	expanded = !expanded;
 	document.getElementById("fab-inner").classList.toggle("icon-arrows-expand-vertical1");
 	document.getElementById("fab-inner").classList.toggle("icon-arrows-shrink-vertical1");
+	let nearest_tier = Array.from(tiers).findIndex(isElementInViewport);
 	for (const tier of tiers) {
 		let tier_items = tier.nextElementSibling;
 		if (!expanded) {
@@ -26,6 +37,10 @@ tier_expand.addEventListener("click", function() {
 			tier.classList.add("active");
 			tier_items.style.maxHeight = tier_items.scrollHeight + "px";
 		}
+	}
+	if (!expanded && !isElementInViewport(tiers[0])) {
+		tiers[nearest_tier].scrollIntoView();
+		window.scrollBy(0, -document.getElementsByClassName("navcontainer")[0].offsetHeight);
 	}
 });
 tier_expand.addEventListener("mouseleave", e => e.target.blur());
